deb2293581b38d7d54d5a17e6e57c0b5b30cb78f
Qasim Iqbal <me@qas.im>
2017-04-24 00:39:01 -0400

    Add Xcode project

---
+++ ChakraCore-Cocoa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ChakraCore-Cocoa
+//
+//  Created by Qasim Iqbal on 2017-04-24.
+//  Copyright © 2017 Qasim Iqbal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ ChakraCore-Cocoa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChakraCore-Cocoa
+//
+//  Created by Qasim Iqbal on 2017-04-24.
+//  Copyright © 2017 Qasim Iqbal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
